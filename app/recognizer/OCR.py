7c6e38373d05625d7e1c1f83cd3c877a02c5a485
pikamanh <hungmanh190204.github@gmail.com>
2025-12-18 18:48:18 +0700

    Fixing mutual Variable in class OCR, remove proccess Focus Image

--- app/recognizer/OCR.py
+++ app/recognizer/OCR.py
@@ -2,7 +2,6 @@ from PIL import Image
 import cv2
 import numpy as np
 
-from app.utils.pre_proccessing import ProccessingImage
 from vietocr.tool.predictor import Predictor
 from vietocr.tool.config import Cfg
 
@@ -14,9 +13,10 @@ class OCR:
         config['device'] = device
 
         self.detector = Predictor(config=config)
-        self.results = {}
-        self.img = None
-        self.rois = {
+
+    def predict(self, img):
+        results = {}
+        rois = {
             "id_number":        None,
             "name":             None,
             "dob":              None,
@@ -27,40 +27,31 @@ class OCR:
             "place_of_residence2": None,
             "date_expired": None
         }
-
-    def predict(self, img):
-        self.img = cv2.resize(img, (640, 640), interpolation=cv2.INTER_AREA)
-        text, out_img = get_text_boxes(self.img)
+        img = cv2.resize(img, (640, 640), interpolation=cv2.INTER_AREA)
+        text, out_img = get_text_boxes(img)
         corner_list = []
         
         for corners_probs in text:
             corners = [int(corner) for corner in corners_probs[:-1]]
-            corner_list.append(corners)
+            if corners[1] > 200:
+                corner_list.append(corners)
 
         corner_list = np.array(corner_list)
         corner_list = corner_list[corner_list[:, 1].argsort()]
 
-        for key, value in zip(self.rois.keys(), corner_list):
-            self.rois[key] = (value[0].item(), value[1].item(), value[6].item(), value[7].item())
+        for key, value in zip(rois.keys(), corner_list):
+            rois[key] = (value[0].item(), value[1].item(), value[6].item(), value[7].item())
         
-        date_expired = self.rois["place_of_residence2"]
-        self.rois["place_of_residence2"] = self.rois["date_expired"]
-        self.rois["date_expired"] = date_expired
+        date_expired = rois["place_of_residence2"]
+        rois["place_of_residence2"] = rois["date_expired"]
+        rois["date_expired"] = date_expired
 
         try:
-            for field, (x1, y1, x2, y2) in self.rois.items():
-                roi_img = self.img[y1:y2, x1:x2]
+            for field, (x1, y1, x2, y2) in rois.items():
+                roi_img = img[y1:y2, x1:x2]
                 roi_img = Image.fromarray(roi_img)
                 text = self.detector.predict(roi_img)
-                self.results[field] = text
-            return self.results
+                results[field] = text
+            return results
         except Exception as e:
             return {"status": "Thông tin trích xuất hiện không đủ. Vui lòng thử lại sau."}
-    
-if __name__ == "__main__":
-    proccessingImage = ProccessingImage()
-    ocr = OCR()
-    cropped_image = proccessingImage.focus_image(r"temp/test1.jpg")
-    test = ocr.predict(cropped_image)
-
-    print(test)
--- app/utils/pre_proccessing.py
+++ app/utils/pre_proccessing.py
@@ -57,72 +57,73 @@ class ProccessingImage:
         return rotated_image
     
     def focus_image(self, img_path):
-        horizontal_lines = []
-        vertical_lines = []
+        # horizontal_lines = []
+        # vertical_lines = []
 
-        angle_threshold_rad = np.deg2rad(20)
-        horizontal_target_rad = np.pi / 2
+        # angle_threshold_rad = np.deg2rad(20)
+        # horizontal_target_rad = np.pi / 2
         img = cv2.imread(img_path)
 
         rotated_image = self.rotate_image(img)
-        canny = cv2.Canny(rotated_image, 70, 150)
-        lines = cv2.HoughLines(canny, 1, np.pi/180, 180)
+        return rotated_image
+        # canny = cv2.Canny(rotated_image, 70, 150)
+        # lines = cv2.HoughLines(canny, 1, np.pi/180, 180)
 
-        # return rotated_image
-        for line in lines:
-            rho, theta = line[0]
+        # # return rotated_image
+        # for line in lines:
+        #     rho, theta = line[0]
 
-            # Kiểm tra đường ngang (gần 90 độ)
-            if abs(theta - horizontal_target_rad) < angle_threshold_rad:
-                horizontal_lines.append((rho, theta))
+        #     # Kiểm tra đường ngang (gần 90 độ)
+        #     if abs(theta - horizontal_target_rad) < angle_threshold_rad:
+        #         horizontal_lines.append((rho, theta))
             
-            # Kiểm tra đường dọc (gần 0 hoặc 180 độ)
-            elif abs(theta) < angle_threshold_rad or abs(theta - np.pi) < angle_threshold_rad:
-                vertical_lines.append((rho, theta))
+        #     # Kiểm tra đường dọc (gần 0 hoặc 180 độ)
+        #     elif abs(theta) < angle_threshold_rad or abs(theta - np.pi) < angle_threshold_rad:
+        #         vertical_lines.append((rho, theta))
 
-        if not horizontal_lines or not vertical_lines:
-            print("Không tìm đủ đường ngang hoặc dọc, hãy thử điều chỉnh ngưỡng Canny hoặc HoughLines.")
-        else:
-            horizontal_lines.sort(key=lambda x: x[0])
-            vertical_lines.sort(key=lambda x: x[0])
+        # if not horizontal_lines or not vertical_lines:
+        #     print("Không tìm đủ đường ngang hoặc dọc, hãy thử điều chỉnh ngưỡng Canny hoặc HoughLines.")
+        # else:
+        #     horizontal_lines.sort(key=lambda x: x[0])
+        #     vertical_lines.sort(key=lambda x: x[0])
 
-        # Lấy đường có rho nhỏ nhất và lớn nhất
-        top_line = horizontal_lines[0]
-        bottom_line = horizontal_lines[-1]
-        left_line = vertical_lines[-1]
-        right_line = vertical_lines[0]
+        # # Lấy đường có rho nhỏ nhất và lớn nhất
+        # top_line = horizontal_lines[0]
+        # bottom_line = horizontal_lines[-1]
+        # left_line = vertical_lines[-1]
+        # right_line = vertical_lines[0]
 
-        corner_tl = self.line_intersection(top_line, left_line)
-        corner_tr = self.line_intersection(top_line, right_line)
-        corner_bl = self.line_intersection(bottom_line, left_line)
-        corner_br = self.line_intersection(bottom_line, right_line)
-        src_points = np.float32([corner_tl, corner_tr, corner_br, corner_bl])
+        # corner_tl = self.line_intersection(top_line, left_line)
+        # corner_tr = self.line_intersection(top_line, right_line)
+        # corner_bl = self.line_intersection(bottom_line, left_line)
+        # corner_br = self.line_intersection(bottom_line, right_line)
+        # src_points = np.float32([corner_tl, corner_tr, corner_br, corner_bl])
 
-        # Tính chiều rộng và chiều cao mới
-        # Chiều rộng = khoảng cách trung bình giữa (tl, tr) và (bl, br)
-        width_top = np.linalg.norm(np.array(corner_tl) - np.array(corner_tr))
-        width_bottom = np.linalg.norm(np.array(corner_bl) - np.array(corner_br))
-        max_width = int(max(width_top, width_bottom))
-        # Chiều cao = khoảng cách trung bình giữa (tl, bl) và (tr, br)
-        height_left = np.linalg.norm(np.array(corner_tl) - np.array(corner_bl))
-        height_right = np.linalg.norm(np.array(corner_tr) - np.array(corner_br))
-        max_height = int(max(height_left, height_right))
+        # # Tính chiều rộng và chiều cao mới
+        # # Chiều rộng = khoảng cách trung bình giữa (tl, tr) và (bl, br)
+        # width_top = np.linalg.norm(np.array(corner_tl) - np.array(corner_tr))
+        # width_bottom = np.linalg.norm(np.array(corner_bl) - np.array(corner_br))
+        # max_width = int(max(width_top, width_bottom))
+        # # Chiều cao = khoảng cách trung bình giữa (tl, bl) và (tr, br)
+        # height_left = np.linalg.norm(np.array(corner_tl) - np.array(corner_bl))
+        # height_right = np.linalg.norm(np.array(corner_tr) - np.array(corner_br))
+        # max_height = int(max(height_left, height_right))
 
-        # Tọa độ đích (hình chữ nhật hoàn hảo)
-        dst_points = np.float32([
-            [0, 0],              # Top-left
-            [max_width - 1, 0],  # Top-right
-            [max_width - 1, max_height - 1], # Bottom-right
-            [0, max_height - 1]  # Bottom-left
-        ])
+        # # Tọa độ đích (hình chữ nhật hoàn hảo)
+        # dst_points = np.float32([
+        #     [0, 0],              # Top-left
+        #     [max_width - 1, 0],  # Top-right
+        #     [max_width - 1, max_height - 1], # Bottom-right
+        #     [0, max_height - 1]  # Bottom-left
+        # ])
 
-        # Lấy ma trận biến đổi
-        M = cv2.getPerspectiveTransform(src_points, dst_points)
+        # # Lấy ma trận biến đổi
+        # M = cv2.getPerspectiveTransform(src_points, dst_points)
 
-        # Áp dụng biến đổi
-        cropped_image = cv2.warpPerspective(rotated_image, M, (max_width, max_height))
+        # # Áp dụng biến đổi
+        # cropped_image = cv2.warpPerspective(rotated_image, M, (max_width, max_height))
 
-        return cropped_image
+        # return cropped_image
 
 if __name__ == "__main__":
     image = cv2.imread(r"temp\temp.jpg")
